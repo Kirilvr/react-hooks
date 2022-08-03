2d11cad13826312c588221f8b4f925d6008844fa
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2022-08-03 14:58:46 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,48 @@
+import { useState, useEffect, cloneElement } from 'react';
+
 export const App = () => {
-  return <div>Hello World!</div>;
+  const [type, setType] = useState('users');
+  const [data, setData] = useState([]);
+  const [pos, setPos] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  // useEffect(() => {
+  //   console.log('render');
+  // });
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, [type]);
+
+  const mouseMoveHandler = (event) => {
+    setPos({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
+
+  useEffect(() => {
+    console.log("I'm Ready!");
+
+    window.addEventListener('mousemove', mouseMoveHandler);
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Ресурс: {type}</h1>
+      <button onClick={() => setType('users')}>Users</button>
+      <button onClick={() => setType('todos')}>Todo</button>
+      <button onClick={() => setType('posts')}>Posts</button>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <pre>{JSON.stringify(pos, null, 2)}</pre>
+    </div>
+  );
 };
