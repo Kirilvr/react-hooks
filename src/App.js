65ab44cc7be4e46358b5ed6e5edbf3c54f9a2d8c
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2022-08-03 16:45:02 +0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
+import { useRef, useEffect, useState } from 'react';
+
 export const App = () => {
-  return <div>Hello World!</div>;
+  const [value, setValue] = useState('initial');
+  const renderCount = useRef(1);
+  const inputRef = useRef(null);
+  const prevValue = useRef('');
+
+  useEffect(() => {
+    renderCount.current++;
+    console.log(inputRef.current.value);
+  });
+
+  useEffect(() => {
+    prevValue.current = value;
+  }, [value]);
+
+  const focus = () => inputRef.current.focus();
+
+  return (
+    <div>
+      <h1>Количество рендеров: {renderCount.current}</h1>
+      <h2>Прошлое состояние: {prevValue.current}</h2>
+      <input
+        ref={inputRef}
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <button className="btn btn-success" onClick={focus}>
+        Фокус
+      </button>
+    </div>
+  );
 };
