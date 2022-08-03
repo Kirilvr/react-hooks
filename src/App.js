ee9d89574a701cdbb9f81ccd862de1fee91997dc
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2022-08-04 02:15:30 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,49 @@
+import { useState, useMemo, useEffect } from 'react';
+
+const complexComputed = (num) => {
+  let i = 0;
+  while (i < 1000000000) i++;
+  return num * 2;
+};
+
 export const App = () => {
-  return <div>Hello World!</div>;
+  const [number, setNumber] = useState(42);
+  const [colored, setColored] = useState(false);
+
+  const styles = useMemo(
+    () => ({ color: colored ? 'darkred' : 'black' }),
+    [colored]
+  );
+
+  const computed = useMemo(() => {
+    return complexComputed(number);
+  }, [number]);
+
+  useEffect(() => {
+    console.log('styles changed');
+  }, [styles]);
+
+  return (
+    <>
+      <h1 style={styles}>Вычисляемое свойство: {computed}</h1>
+      <button
+        className="btn btn-success"
+        onClick={() => setNumber((prev) => prev + 1)}
+      >
+        Добавить
+      </button>
+      <button
+        className="btn btn-danger"
+        onClick={() => setNumber((prev) => prev - 1)}
+      >
+        Убрать
+      </button>
+      <button
+        className="btn btn-warning"
+        onClick={() => setColored((prev) => !prev)}
+      >
+        Изменить
+      </button>
+    </>
+  );
 };
