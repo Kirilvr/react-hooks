8ed1ad40625c533a0d98383e1b8b86b633b2e87f
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2022-08-04 02:37:55 +0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
+import { useCallback, useState } from 'react';
+import { ItemList } from './ItemList';
+
 export const App = () => {
-  return <div>Hello World!</div>;
+  const [colored, setColored] = useState(false);
+  const [count, setCount] = useState(1);
+
+  const styles = {
+    color: colored ? 'darkred' : 'black',
+  };
+
+  const generateItemsFromAPI = useCallback(() => {
+    return new Array(count).fill('').map((_, i) => `Элемент ${i + 1}`);
+  }, [count]);
+
+  return (
+    <>
+      <h1 style={styles}>Количество элементов: {count}</h1>
+      <button
+        className="btn btn-success"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Добавить
+      </button>
+      <button
+        className="btn btn-warning"
+        onClick={() => setColored((prev) => !prev)}
+      >
+        Изменить
+      </button>
+      <ItemList getItems={generateItemsFromAPI} />
+    </>
+  );
 };
