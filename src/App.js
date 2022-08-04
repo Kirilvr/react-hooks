cdcd11a6e223ae487a15567116a561a8e0209483
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2022-08-04 16:09:06 +0300

    custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,50 @@
+import React, { useState, useEffect } from 'react';
+
+const useLogger = (value) => {
+  useEffect(() => {
+    console.log('Value target: ', value);
+  }, [value]);
+};
+
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const clear = () => setValue('');
+
+  return {
+    bind: { value, onChange },
+    value,
+    clear,
+  };
+};
+
 export const App = () => {
-  return <div>Hello World!</div>;
+  const input = useInput('');
+  const lastName = useInput('');
+
+  useLogger(input.value);
+
+  return (
+    <div className="containet pt-3">
+      <input type="text" {...input.bind} />
+      <input type="text" {...lastName.bind} />
+      <button
+        className="btn btn-warning"
+        onClick={() => {
+          input.clear();
+          lastName.clear();
+        }}
+      >
+        Clean
+      </button>
+      <hr />
+      <h1>
+        {input.value} {lastName.value}
+      </h1>
+    </div>
+  );
 };
